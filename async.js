b9e2a72d0b6be4ada038d52ffbd2634e90032374
ChamodSathsara <sathsarawapc@gmail.com>
2024-07-24 16:42:59 +0530

    Sync & async

---
+++ async.js
@@ -0,0 +1,17 @@
+function oneto5() {
+  console.log(1);
+  console.log(2);
+  console.log(3);
+  console.log(4);
+  console.log(5);
+}
+
+console.log(`start`);
+setTimeout(oneto5, 3000);
+console.log(6);
+console.log(7);
+console.log(8);
+console.log(9);
+console.log(10);
+
+// paralaly eka para dekak run wenna
