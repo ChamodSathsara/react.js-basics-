a29d1cbbdc26a4e270715790d6c25c81cf15643d
ChamodSathsara <sathsarawapc@gmail.com>
2024-07-25 14:39:16 +0530

    react basics

---
+++ ES-Modules/extra.js
@@ -0,0 +1,7 @@
+// using import export keyword
+
+export const text = `this is from Extra.js`;
+
+export function hello(name) {
+  console.log(`hello ${name} !`);
+}
---
+++ ES-Modules/index.js
@@ -0,0 +1,4 @@
+import { text, hello } from "./extra.js";
+console.log(text);
+
+console.log(`text log from index.js`);
